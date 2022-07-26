0f996bc129f28d8d12ad70b754fba0475912f6f9
Quinnten Silveira-Charbonnier <charbonnierq19@gmail.com>
2022-07-26 11:24:05 -0400

    Finished Gatsby tutorial 5

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,6 +8,7 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-image`,
+    `gatsby-plugin-mdx`,
     {
       resolve: "gatsby-source-filesystem",
       options: {

34eb9cb93ff9c8a3a7a0865e925e22383cc81bcd
Quinnten Silveira-Charbonnier <charbonnierq19@gmail.com>
2022-08-02 16:31:56 -0400

    added the twitter share button to all the blog posts

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { graphql } from "gatsby";
+import { graphql, Script } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx";
 import { GatsbyImage, getImage } from "gatsby-plugin-image";
 import Layout from "../../components/layout";
@@ -12,6 +12,21 @@ const BlogPost = ({ data }) => {
       <p>Posted: {data.mdx.frontmatter.date}</p>
       <GatsbyImage image={image} alt={"image"} />
       <MDXRenderer>{data.mdx.body}</MDXRenderer>
+      <a
+        href="https://twitter.com/share?ref_src=twsrc%5Etfw"
+        className="twitter-share-button"
+        data-size="large"
+        data-text="Check out this article from Color Theory!"
+        data-hashtags="ColorTheory"
+        data-show-count="false"
+      >
+        Tweet
+      </a>
+      <Script
+        async
+        src="https://platform.twitter.com/widgets.js"
+        charset="utf-8"
+      ></Script>
     </Layout>
   );
 };
